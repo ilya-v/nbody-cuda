65e447d6fb3e5a94f5b47a6202ccfa5cd1a1bfd3
Ilya Volkovets <ivolkovets.contractor@libertyglobal.com>
2018-08-19 19:29:45 +0200

    Generate the initial configuration

--- nbody.cu
+++ nbody.cu
@@ -19,148 +19,226 @@ double getTimer() {
 #define BLOCK_SIZE 256
 
 typedef struct {
+    const char *fmt;
+    void *ptr;
+} record_t;
+
+bool read_param(const char *line, const char *fmt, void *dest) {
+    char
+            name[2564]  = {0,},
+            type[16]    = {0,},
+            key[256]    = {0,},
+            value[256]  = {0,};
+    return  (2 == sscanf(fmt, "%63s %8s", name, type)) &&
+            (2 == sscanf(line, " %[a-z_A-Z0-9] %*[=] %s", key, value)) &&
+            (0 == strncmp(name, key, sizeof(key))) &&
+            (1 == sscanf(value, type, dest));
+}
+
+const char* print_param(const char *fmt, void *ptr) {
+    static char out[256];
+    char *type = NULL;
+    return
+        (type = strchr(fmt, '%')) && (
+            (strstr(type, "lf") && sprintf(out, fmt, *(double*)ptr) > 0)   ||
+            (strstr(type, "lg") && sprintf(out, fmt, *(double*)ptr) > 0)   ||
+            (strstr(type, "le") && sprintf(out, fmt, *(double*)ptr) > 0)   ||
+            (strstr(type, "u")  && sprintf(out, fmt, *(unsigned*)ptr) > 0) ||
+            (strstr(type, "d")  && sprintf(out, fmt, *(int*)ptr) > 0)      ||
+            (strstr(type, "ld") && sprintf(out, fmt, *(long*)ptr) > 0)     ||
+            (strstr(type, "s")  && sprintf(out, fmt, (char*)ptr) > 0))  ?
+                out : fmt;
+}
+
+typedef struct {
 
     unsigned
-        n_steps,
-        n_steps_for_report,
-        n_steps_for_output;
+            n_steps,
+            n_steps_for_report,
+            n_steps_for_output;
 
     double
-        dt_max,
-        dv_max,
-        dt_start,
-        t_start,
-        r2_eps;
+            dt_max,
+            dv_max,
+            dt_start,
+            t_start,
+            r2_eps;
+
+    const char *initial_config;
+    double
+            config_a,
+            config_r,
+            config_ang_vel,
+            config_rand_vel;
+
+    unsigned random_seed;
 } params_t;
 
 
 static params_t params = {
-    .n_steps = 1000*1000,
-    .n_steps_for_report = 100,
-    .n_steps_for_output = 1000,
-    .dt_max = 0.01,
-    .dv_max = 0.01,
-    .dt_start = 0.001,
-    .t_start = 0,
-    .r2_eps = 0.01
+        .n_steps = 1000*1000,
+        .n_steps_for_report = 100,
+        .n_steps_for_output = 1000,
+        .dt_max = 0.01,
+        .dv_max = 0.01,
+        .dt_start = 0.001,
+        .t_start = 0,
+        .r2_eps = 0.01,
+        .initial_config = "",
+        .config_a = 1.0,
+        .config_r = 10.0,
+        .config_ang_vel = 0.1,
+        .config_rand_vel = 0.1,
+        .random_seed = 102030,
+
 };
 
-typedef struct  {
-    const char *name, *type;
-    void *ptr;
-} param_rec_t;
-
-static const param_rec_t param_recs[] = {
-    {   "n_steps",              "%u",   &params.n_steps             },
-    {   "n_steps_for_report",   "%u",   &params.n_steps_for_report  },
-    {   "n_steps_for_output",   "%u",   &params.n_steps_for_output  },
-    {   "dt_max",               "%lf",  &params.dt_max              },
-    {   "dv_max",               "%lf",  &params.dv_max              },
-    {   "dt_start",             "%lf",  &params.dt_start            },
-    {   "t_start",              "%lf",  &params.t_start             },
-    {   "r2_eps",               "%lf",  &params.r2_eps              },
-    {   NULL,   }
+static const record_t param_recs[] = {
+        {   "n_steps                 %u",   &params.n_steps             },
+        {   "n_steps_for_report      %u",   &params.n_steps_for_report  },
+        {   "n_steps_for_output      %u",   &params.n_steps_for_output  },
+        {   "dt_max                  %lf",  &params.dt_max              },
+        {   "dv_max                  %lf",  &params.dv_max              },
+        {   "dt_start                %lf",  &params.dt_start            },
+        {   "t_start                 %lf",  &params.t_start             },
+        {   "r2_eps                  %lf",  &params.r2_eps              },
+        {   "initial_config          %s",    params.initial_config      },
+        {   "config_a                %lf",  &params.config_a            },
+        {   "config_r                %lf",  &params.config_r            },
+        {   "config_ang_vel          %lf",  &params.config_ang_vel      },
+        {   NULL,   }
 };
-
-bool try_read_param(const char *line, const param_rec_t *rec) {
-    const char *key = strstr(line, rec->name);
-    const char *key_end = key? (key + strlen(rec->name)) : NULL;
-    const bool key_found = key_end &&
-        (*key_end == '=' || isspace(*key_end));
-
-    const char *value = key_found? strchr(key, '=') : NULL;
-    return  value?  (sscanf(value + 1, rec->type, rec->ptr) == 1) : false;
-}
 
 void read_params() {
-
     FILE *fparam = fopen("params.txt", "r");
     if (!fparam)
         return;
 
     const unsigned max_line_len = 256;
-    for(char buf[max_line_len] = {'\x0',}; fgets(buf, max_line_len, fparam);)
-        for (const param_rec_t *rec = param_recs; rec->name; rec++)
-            if (try_read_param(buf, rec))
+    for(char buf[max_line_len] = {0,}; fgets(buf, max_line_len, fparam);)
+        for (const record_t *rec = param_recs; rec->fmt; rec++)
+            if (read_param(buf, rec->fmt, rec->ptr))
                 break;
 
     fclose(fparam);
 }
 
-void show_params(const bool to_stdout) {
-    FILE *fo = to_stdout? stdout : stderr;
-    for (const param_rec_t *rec = param_recs; rec->name; rec++) {
-        (0 == strcmp("%lf", rec->type))?
-            fprintf(fo, "#%s = %lg\n", rec->name, *(double*)rec->ptr) :
-        (0 == strcmp("%u", rec->type))?
-            fprintf(fo, "#%s = %u\n", rec->name, *(unsigned*)rec->ptr) :
-            fprintf(fo, "#%s = unknown\n", rec->name);
-    }
+void print_recs(const record_t param_recs[], FILE *fout,
+                const char *pre, const char *post) {
+    for (const record_t *rec = param_recs; rec->fmt; rec++)
+        fprintf(fout, "%s%s%s", pre, print_param(rec->fmt, rec->ptr), post);
 }
 
 typedef struct  {
     unsigned step;
     double t, dt, ep, ek, etot, I;
+    long rng;
+
 } status_t;
 status_t status = { 0, };
 
-typedef struct {
-    const char *fmt;
-    void *ptr;
-} status_rec_t;
-
-static const status_rec_t status_recs[] = {
-    { "step %8u",        &status.step    },
-    { "time %16.8lf",    &status.t       },
-    { "dt   %16.8le",    &status.dt      },
-    { "ep   %16.8le",    &status.ep      },
-    { "ek   %16.8le",    &status.ek      },
-    { "etot %16.8le",    &status.etot    },
-    { "I    %16.8le",    &status.I       },
+static const record_t status_recs[] = {
+    { "step %8u",       &status.step    },
+    { "time %16.8lf",   &status.t       },
+    { "dt   %16.8le",   &status.dt      },
+    { "ep   %16.8le",   &status.ep      },
+    { "ek   %16.8le",   &status.ek      },
+    { "etot %16.8le",   &status.etot    },
+    { "I    %16.8le",   &status.I       },
+    { "rng  %8u",       &status.rng     },
     { NULL  }
 };
 
-void print_double(const char *fmt, void *p) { printf(fmt, *(double*)p); }
-void print_int   (const char *fmt, void *p) { printf(fmt, *(int*)p);    }
-typedef struct {
-    const char *type;
-    void (*print_f)(const char *fmt, void *p);
-} print_rec_t;
-static const print_rec_t print_recs[] = {
-    { "lf", print_double    },
-    { "lg", print_double    },
-    { "le", print_double    },
-    { "u",  print_int       },
-    { "d",  print_int       },
-    { NULL  }
-};
-
-void status_print_header() {
-    for (const status_rec_t *rec = status_recs; rec->fmt; rec++) {
+void status_print_header(const record_t records[]) {
+    for (const record_t *rec = records; rec->fmt; rec++) {
         const int
-            width = atoi(strchr(rec->fmt, '%') + 1),
-            n = (int)(strchr(rec->fmt, ' ') - rec->fmt);
+                width = atoi(strchr(rec->fmt, '%') + 1),
+                n = (int)(strchr(rec->fmt, ' ') - rec->fmt);
         printf("%*s#%*.*s", width - n, " ", n, n, rec->fmt );
     }
     printf("\n");
 }
 
-void status_print() {
-    for (const status_rec_t *rec = status_recs; rec->fmt; rec++) {
-        char *type = NULL;
-        strtod(strchr(rec->fmt, '%') + 1, &type);
-        for (const print_rec_t * prec = print_recs; prec->type; prec++) {
-            if(strncmp(type, prec->type, strlen(prec->type)) == 0) {
-                prec->print_f(strchr(rec->fmt, '%') - 1, rec->ptr);
-                break;
-            }
-        }
-    }
+void status_print(const record_t records[]) {
+    for (const record_t *rec = status_recs; rec->fmt; rec++)
+        printf(" %s", print_param(strchr(rec->fmt, '%'), rec->ptr));
     printf("\n");
 }
 
 
+void generate_initial_config(params_t *params) {
+    if (!(params->initial_config) || !params->initial_config[0])
+        return;
+
+    double rand(long *rng_state);
+
+    FILE *fout = fopen("input.txt");
+
+    if (fout && 0 == strcmp("sphere", params->initial_config)) {
+        const unsigned Ns = (unsigned)(params->config_r / params->config_a) + 1;
+        for (int i = -Ns; i <= Ns; i++)
+        for (int j = -Ns; j <= Ns; j++)
+        for (int k = -Ns; k <= Ns; k++) {
+            const double
+                x = params->config_a * i,
+                y = params->config_a * j,
+                z = params->config_a * k;
+            if (x*x + y*y + z*z > params->config_r * params->config_r)
+                continue;
+            const double
+                vx = - params->config_ang_vel * y,
+                vy = params->config_ang_vel * x,
+                vz = 0;
+            const double
+                vrx = 2 * (rand(&status.rng) - 0.5) * params->config_rand_vel,
+                vry = 2 * (rand(&status.rng) - 0.5) * params->config_rand_vel,
+                vrz = 2 * (rand(&status.rng) - 0.5) * params->config_rand_vel;
+
+            fprintf(fout, "%lf %lf %lf %lf %lf %lf\n",
+                    x,y,z, vx + vrx, vy + vry, vz + vrz);
+        }
+    }
+    if (fout)
+        fclose(fout);
+}
+
+
 typedef struct { double x, y, z, vx, vy, vz; } Particle;
+
+
+unsigned read_initial_config(Particle **particles, Particle **old_particles) {
+    int N = 0;
+    FILE *fin = fopen("input.txt", "r");
+    if (!fin) {
+        printf("Cannot open input.txt\n");
+        return (unsigned)-1;
+    }
+
+    double t1, t2, t3, t4, t5, t6;
+    for (; 6 == fscanf(fin, "%lf  %lf  %lf  %lf  %lf  %lf\n",
+                            &t1, &t2, &t3, &t4, &t5, &t6);
+        N++);
+
+    rewind(fin);
+    *particles = (Particle *) malloc(N*sizeof(Particle));
+    *old_particles = (Particle *) malloc(N*sizeof(Particle));
+
+    unsigned i = 0;
+    for (; i < N; i++)
+    if (6 != fscanf(fin, "%lf  %lf  %lf  %lf  %lf  %lf\n",
+            &(*particles)[i].x,  &(*particles)[i].y,  &(*particles)[i].z,
+            &(*particles)[i].vx, &(*particles)[i].vy, &(*particles)[i].vz))
+         break;
+
+    fclose(fin);
+
+    if (i < N) {
+        printf("Cannot read input.txt: %u lines from %u\n", i, N);
+        return (unsigned)-1;
+    }
+    return N;
+}
+
 
 
 void status_update( const unsigned N,
@@ -187,7 +265,7 @@ void status_update( const unsigned N,
 
 void particles_center(const unsigned N, Particle particles[]) {
 
-    double  x= 0,   y = 0,  z = 0;
+    double  x = 0,  y = 0,  z = 0;
     double  px = 0, py = 0, pz = 0;
     for (Particle *p = particles; p < particles + N; p++) {
         px += p->vx;    py += p->vy;    pz += p->vz;
@@ -247,57 +325,21 @@ void calcPotential(Particle *p, double *u, unsigned N, double r2_eps) {
 
 int main(const int argc, const char** argv) {
 
-    if (argc >= 2 && strstr(argv[1], "-p"))
-    {
-        show_params(true);
+    if (argc >= 2 && strstr(argv[1], "-p")) {
+        print_recs(param_recs, stdout, "#", "\n");
         return 0;
     }
 
     read_params();
-    show_params(false);
+    print_recs(param_recs, stdout, "#", "\n");
+
+    status.rng = -(long)params.random_seed;
+    generate_initial_config(&params);
 
     Particle
         *particles = NULL,
         *old_particles = NULL;
-
-    unsigned N = 0;
-
-    {
-        FILE *fin = fopen("input.txt", "r");
-        if (!fin) {
-            printf("Cannot open input.txt\n");
-            return -1;
-        }
-
-        {
-            double t1, t2, t3, t4, t5, t6;
-            for (; 6 == fscanf(fin, "%lf  %lf  %lf  %lf  %lf  %lf\n",
-                               &t1, &t2, &t3, &t4, &t5, &t6);
-                   N++);
-        }
-
-        rewind(fin);
-        particles = (Particle *) malloc(N*sizeof(Particle));
-        old_particles = (Particle *) malloc(N*sizeof(Particle));
-        unsigned i = 0;
-        for (; i < N; i++)
-            if (6 != fscanf(fin, "%lf  %lf  %lf  %lf  %lf  %lf\n",
-                                    &particles[i].x,
-                                    &particles[i].y,
-                                    &particles[i].z,
-                                    &particles[i].vx,
-                                    &particles[i].vy,
-                                    &particles[i].vz))
-                break;
-
-        fclose(fin);
-
-        if (i < N) {
-            printf("Cannot read input.txt: %u lines from %u\n", i, N);
-            return -1;
-        }
-    }
-
+    unsigned N = read_initial_config(&particles, &old_particles);
     particles_center(N, particles);
 
     const int nBlocks = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
@@ -353,7 +395,7 @@ int main(const int argc, const char** argv) {
             cudaMemcpy(u, d_u, N*sizeof(double), cudaMemcpyDeviceToHost);
 
             status_update(N, particles, u);
-            status_print();
+            status_print(status_recs);
         }
 
         if (status.step % params.n_steps_for_output == 0) {
